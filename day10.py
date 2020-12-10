73ede66e6d9e42d6bedd4e7387ef593b5107ee81
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-12-10 20:07:55 +0100

    day10, part2

--- day10.py
+++ day10.py
@@ -5,20 +5,45 @@ with open("day10.input.txt") as f:
     assert len(set(adapters)) == len(adapters)
 
 def run():
-    count_1 = 1  # pre-counted: outlet -> adapter
+    count_1 = 0
     count_3 = 1  # pre-counted: adapter -> phone
 
-    for b, c in zip(adapters, adapters[1:]):
-        assert 0 <= c - b <= 3, (b, c)
+    # solution to part 2 -- we start out with 1 combination and multiply by 2
+    # every time we try to omit a jolt
+    combinations = 1
+    jolt_1_len = 0  # current streak of 1-jolt differences
+    a = 0
+    for b in adapters:
+        jolt = b - a
 
-        if c - b == 3:
+        if jolt == 3:
+            jolt_1_len = 0
             count_3 += 1
 
-        if c - b == 1:
+        elif jolt == 1:
+            jolt_1_len += 1
+            if 1 < jolt_1_len < 4:
+                # a length-n sequence of 1-jolt increments can generally have
+                # all but the last adapter omitted: 2 ^ (n-1)
+                combinations *= 2
+            elif jolt_1_len == 4:
+                # length-4 jolt groups cannot have all three adapters omitted
+                # at the same time:
+                # 2 ^ (n-1) - 1 = 7
+                #
+                # unfortunately we only have a single integer so we need to
+                # undo a bit of previous calculation
+                combinations /= 4
+                combinations *= 7
             count_1 += 1
+        else:
+            assert False, (a, b)
 
-    return count_1 * count_3
+        a = b
 
-part1 = run()
+    return count_1 * count_3, combinations
 
-print("part 1: {}".format(run()))
+part1, part2 = run()
+
+print("part 1: {}".format(part1))
+print("part 2: {}".format(part2))
